059674189214402454d70ac2a0c013e8b2c788b9
Ignas Damunskis <ignas.damunskis@esl-asia.com>
2022-06-02 10:11:24 +0200

    .

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 const { openPlaywrightSite } = require('../helpers/example.js');
 
-test('verify playwright site', async ({ page }) => {
+test('verify playwright site title', async ({ page }) => {
   await openPlaywrightSite({ page })
   const title = page.locator('header.hero .hero__title');
   await expect(title).toContainText('enables reliable end-to-end testing for modern web apps');
